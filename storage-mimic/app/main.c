1e1b46ec11535dbf1808215551f6138d8a3758e0
Sebastian Carpenter <SebastianRCarpenter@gmail.com>
2025-04-24 17:15:18 -0600

    cannot get the master read to work
    I think I need to rework i2c

--- storage-mimic/app/main.c
+++ storage-mimic/app/main.c
@@ -1,5 +1,8 @@
-#include <msp430fr2355.h>
+#include "intrinsics.h"
+#include <msp430fr2310.h>
 #include <stdint.h>
+
+#include "../../i2c/src/i2c.h"
 
 int main(void) {
 
@@ -7,21 +10,30 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;
 
     // hearbeat LED
-    P2SEL0 &= !BIT0;
-    P2SEL1 &= !BIT0;
-    P2OUT &= ~BIT0;
-    P2DIR |= BIT0;
+    P1SEL0 &= !BIT0;
+    P1SEL1 &= !BIT0;
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+
+    init_i2c_pins();
 
     // turn on I/O
     PM5CTL0 &= ~LOCKLPM5;
 
-    uint16_t i;
-    while(1)
-    {
-        // toggle heartbeat
-        P2OUT ^= BIT0;
+    // 832 / 16 = 52
+    uint8_t buffer[832];
+    configure_i2c_slave(0x48, buffer, 16);
 
-        // delay
-        for(i = 0; i < 10000; i++);
+    __enable_interrupt();
+
+    volatile uint8_t previous_value = *buffer;
+    while (1)
+    {
+        // toggle heartbeat if SCL is low
+        if (*buffer != previous_value)
+        {
+            previous_value = *buffer;
+            P1OUT ^= BIT0;
+        }
     }
 }
